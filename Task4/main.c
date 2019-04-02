0ba7b68fd96563bff32960bf082f65a004bc55d2
MahmoodNagy <30374414+MahmoodNagy@users.noreply.github.com>
2019-04-02 16:11:59 +0200

    Add files via upload

---
+++ Task4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,a,b;
+    printf("Please enter your number: ");
+    scanf("%i",&x);
+    y=x*x;
+    printf("The result is: %i\n",y);
+
+    return 0;
+}
